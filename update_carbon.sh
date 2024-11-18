d6f14b86255e5fa640e223eb6dd3551f9a19dd20
nuGGGz <admin@walkthewalk.gg>
2024-11-18 14:10:31 -0600

    Update update_carbon.sh

--- update_carbon.sh
+++ update_carbon.sh
@@ -2,13 +2,13 @@
 
 # Variables
 CARBON_URL="https://github.com/CarbonCommunity/Carbon/releases/download/production_build/Carbon.Linux.Release.tar.gz"
-CARBON_ARCHIVE="/home/steam/Carbon.Linux.Release.tar.gz"
-RUST_DATA_DIR="/home/steam/rust_server/RustDedicated_Data"
+CARBON_ARCHIVE="/home/rust/Carbon.Linux.Release.tar.gz"
+RUST_DATA_DIR="/home/rust/rust_server/RustDedicated_Data"
 ENV_SCRIPT="$RUST_DATA_DIR/carbon/tools/environment.sh"
 
 # Ensure script is run as 'steam' user
-if [ "$(whoami)" != "steam" ]; then
-    echo "This script must be run as the 'steam' user. Exiting."
+if [ "$(whoami)" != "rust" ]; then
+    echo "This script must be run as the 'rust' user. Exiting."
     exit 1
 fi
 
